b10ae50c1ab9d146c828386d7b38fe3671de81ef
silveiraneto <silveiraneto@b378b714-6931-0410-8167-936cd852e8ac>
2007-05-27 19:50:13 +0000

    Adding suport for static serving files in django.

--- trunk/urls.py
+++ trunk/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     (r'^polls/(?P<poll_id>\d+)/$', 'foiaqui.polls.views.detail'),
     (r'^polls/(?P<poll_id>\d+)/results/$', 'foiaqui.polls.views.results'),
     (r'^polls/(?P<poll_id>\d+)/vote/$', 'foiaqui.polls.views.vote'),
+    (r'^mediafiles/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/silveira/Desktop/foiaqui/media/'}),
     (r'^admin/', include('django.contrib.admin.urls')),
 )
