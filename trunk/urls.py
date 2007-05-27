31ca6f850b65936ec689e2c33eb8fa9dea4c6f3f
silveiraneto <silveiraneto@b378b714-6931-0410-8167-936cd852e8ac>
2007-05-27 01:41:52 +0000

    fixed names, where was "fa" now is "foiaqui".

--- trunk/urls.py
+++ trunk/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^polls/$', 'fa.polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'fa.polls.views.detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'fa.polls.views.results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'fa.polls.views.vote'),
+    (r'^polls/$', 'foiaqui.polls.views.index'),
+    (r'^polls/(?P<poll_id>\d+)/$', 'foiaqui.polls.views.detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$', 'foiaqui.polls.views.results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$', 'foiaqui.polls.views.vote'),
     (r'^admin/', include('django.contrib.admin.urls')),
 )
