434ba8fc27e894011e2e1a9029a4096f58766c6e
silveiraneto <silveiraneto@b378b714-6931-0410-8167-936cd852e8ac>
2007-06-01 18:09:58 +0000

    just few minor changes

--- trunk/urls.py
+++ trunk/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     (r'^polls/(?P<poll_id>\d+)/$', 'foiaqui.polls.views.detail'),
     (r'^polls/(?P<poll_id>\d+)/results/$', 'foiaqui.polls.views.results'),
     (r'^polls/(?P<poll_id>\d+)/vote/$', 'foiaqui.polls.views.vote'),
-    (r'^mediafiles/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/silveira/Desktop/foiaqui/media'}),
+    (r'^mediafiles/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/lab2/Desktop/foiaqui/media'}),
     (r'^admin/', include('django.contrib.admin.urls')),
     (r'^crimes/xml', 'foiaqui.crimes.views.index'),
     (r'^$', 'foiaqui.core.views.index')
