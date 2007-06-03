52c07c0218aa9c1d34cb31bce963acd48dfc0830
silveiraneto <silveiraneto@b378b714-6931-0410-8167-936cd852e8ac>
2007-06-03 16:55:17 +0000

    When you click in a put, in a point that isn't nothing. It creates a new marker. If you have allready created a new marker in somewhere else, it will jump to where you clicked for last.

--- media/maps.js
+++ media/maps.js
@@ -1,5 +1,6 @@
 //<![CDATA[
 
+// Creates a new incident marker.
 function mk_marker(point, desctext, when, mobility, quantity, thief, weapon, period){
 	var icon = new GIcon();
 	icon.image = "mediafiles/finger.png";
@@ -10,6 +11,7 @@ function mk_marker(point, desctext, when, mobility, quantity, thief, weapon, per
 	icon.infoWindowAnchor = new GPoint(5, 1);
 
 	var marker = new GMarker(point, icon);
+	marker.kind = "INCIDENT";
 
 	var desc_html = "<img src=\"/mediafiles/talking.png\"> <strong>Como foi:</strong><br /> <div class=\"desc\">"+desctext+"</div><img src=\"/mediafiles/clock.png\"> <strong>Quando foi:</strong><br /><div class=\"desc\">"+when+"</div>"
 	var open_icon = "<img src=\"/mediafiles/"
@@ -17,12 +19,20 @@ function mk_marker(point, desctext, when, mobility, quantity, thief, weapon, per
 
 	var icons_html = "<center>"+open_icon+mobility+close_icon+"   "+ open_icon+quantity+close_icon +"   "+ open_icon+thief+close_icon +"   "+ open_icon+weapon+close_icon +"   "+ open_icon+period+close_icon+"</center>"
 
-	GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(desc_html+icons_html);});
+	GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(desc_html+icons_html);alert('old');});
 	return marker;
 }
 
-// <textarea rows="10" cols="30"> The cat was playing in the garden. </textarea>
 
+function mk_input(overlay, point){
+	// If the click was in a empty region
+	if (overlay == null){
+		var new_input = new GMarker(point);
+		map.addOverlay(new_input);
+	}
+}
+
+// The Main funcion
 function load() {
 	if (GBrowserIsCompatible()) {
 		var map = new GMap2(document.getElementById("map"));
@@ -46,6 +56,30 @@ function load() {
 				map.addOverlay(marker);
 			}
 		});
+
+
+		// New input marker
+		var old_marker = null;
+		GEvent.addListener(map, "click", function(overlay, point) {
+			if (overlay==null) {
+				if(old_marker != null){
+					map.removeOverlay(old_marker);
+				}
+				var input_marker = new GMarker(point, {draggable: true});
+				old_marker = input_marker;
+				map.addOverlay(input_marker);
+			}
+ 		});
+
+
+// 		GEvent.addListener(map, "click", function(overlay, point) {
+// 			if (overlay) {
+// 				map.removeOverlay(overlay);
+// 			} else {
+// 				map.addOverlay(new GMarker(point));
+// 			}
+// 		});
+
 	}
 }
 //]]>
